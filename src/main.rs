f0949edb6737030aebfd55c6f065050a313182cc
tonymushah <tonyramanantsalama@gmail.com>
2024-11-26 12:03:51 +0300

    chapter_2

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,16 @@
-use std::path::PathBuf;
+use clap::Parser;
 
-use clap::{Parser, Subcommand};
-
-/// My first clap cli
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(version, about, long_about = None, next_line_help = true)]
 struct Cli {
-    name: Option<String>,
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-    #[command(subcommand)]
-    command: Option<Commands>
+    #[arg(long)]
+    two: String,
+    #[arg(long)]
+    one: String,
 }
 
-#[derive(Subcommand)]
-enum Commands {
-    Test {
-        #[arg(short, long)]
-        list: bool
-    }
-}
 fn main() {
     let cli = Cli::parse();
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        None => {}
-    }
+    println!("two: {:?}", cli.two);
+    println!("one: {:?}", cli.one);
 }
