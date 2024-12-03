3ccf065dd4d90061838aa4494b569b2b2927a898
tonymushah <tonyramanantsalama@gmail.com>
2024-12-03 07:48:05 +0300

    enum values

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None, propagate_version = true)]
 struct Cli {
-    #[arg(default_value_t = 2024)]
-    port: u16,
+    /// What mode to run the program in
+    #[arg(value_enum)]
+    mode: Mode,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Mode {
+    /// Run swiftly
+    Fast,
+    /// Crawl slowly but steadily
+    ///
+    /// Ignored paragraph
+    Slow,
 }
 
 fn main() {
     let cli = Cli::parse();
-    println!("port: {:?}", cli.port);
+    match cli.mode {
+        Mode::Fast => {
+            println!("Hare");
+        }
+        Mode::Slow => {
+            println!("Tortoise");
+        }
+    }
 }
